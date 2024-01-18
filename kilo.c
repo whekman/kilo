d76b9e91d1585a43a8775479c7746fdfb13a4b01
Willem Hekman <hekmanwillem@gmail.com>
2024-01-18 18:22:01 +0100

    started enabling raw mode

--- kilo.c
+++ kilo.c
@@ -2,7 +2,34 @@
 // Kilo: A text-editor written in <1000 lines of C
 // as originally written by Antirez
 
-int main(){
+#include <unistd.h>
+#include <stdio.h>
+#include <termios.h>
 
+void enableRawMode() {
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+
+	// bitwise AND with the bitwise NOT of ECHO
+	// sets the bit of echo to 0 (4th bit)
+	raw.c_lflag &= ~(ECHO);
+
+	// setting with TCSAFLUSH
+	// waits for pending output, discards input
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+
+	enableRawMode();
+
+	char c;
+
+	// Note: STDIN_FILENO is a macro thatll evaluate to 0;
+	// the FD/syscall value for stdin
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+	printf("\nI got:  %c\n", c);
 	return 0;
 }
