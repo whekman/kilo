45dc8b4db9e8769af35d3abf46b24e19bc62f607
Willem Hekman <hekmanwillem@gmail.com>
2024-01-19 21:07:40 +0100

    disabled ia CRNL

--- kilo.c
+++ kilo.c
@@ -4,19 +4,53 @@
 
 #include <unistd.h>
 #include <stdio.h>
+
+// ECHO, ICANON, ISIG, IXON, IEXTEN
 #include <termios.h>
 
-void enableRawMode() {
-	struct termios raw;
+// atexit
+#include <stdlib.h>
 
-	tcgetattr(STDIN_FILENO, &raw);
+// iscntrl
+#include <ctype.h>
 
-	// bitwise AND with the bitwise NOT of ECHO
-	// sets the bit of echo to 0 (4th bit)
-	raw.c_lflag &= ~(ECHO);
+struct termios orig_termios;
 
-	// setting with TCSAFLUSH
+void disableRawMode(){
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+};
+
+void enableRawMode(){
+
+	tcgetattr(STDIN_FILENO, &orig_termios);
+
+	// called after returning from main
+	// comes from stdlib
+	atexit(disableRawMode);
+	
+	struct termios raw = orig_termios;
+
+	// c_lflag: miscellaneous flag bits
+	// disable:
+	// echoing (4th bit) and
+	// canonical mode (2nd bit; waiting for return)
+	// signalling (SIGINT, SIGSTP)
+	// legacy ctrl-V funk (literal esc sequences)
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+	// likewise disable
+	// ctrl-S = XOFF - stops data transmission
+	// ctrl-Q = XON - opens transmission again
+	// converting \r (13) to \n (10)
+	raw.c_iflag &= ~(ICRNL | IXON);
+
+	// disable
+	// output conversion of \n to \r\n
+	raw.c_oflag &=~(OPOST);
+
+	// applies the new settings;
 	// waits for pending output, discards input
+	// (TCSAFLUSH)
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -25,11 +59,20 @@ int main() {
 	enableRawMode();
 
 	char c;
+	//printf("%x\n", ICANON);
 
 	// Note: STDIN_FILENO is a macro thatll evaluate to 0;
 	// the FD/syscall value for stdin
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 's'){
 
-	printf("\nI got:  %c\n", c);
+		if(iscntrl(c)){
+			printf("%d (ctrl)\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+
+	}
+
+	printf("\n Exit (s)\n");
 	return 0;
 }
